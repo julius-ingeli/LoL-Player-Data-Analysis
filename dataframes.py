5a492eb935d292af765288f9f3cf6324671f73bb
Julius Ingeli <93543349+julius-ingeli@users.noreply.github.com>
2022-04-14 17:39:18 +0200

    Delete dataframes.py

--- dataframes.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib as plt
-
-
-playerinfoDF = pd.read_csv("/home/sldsnk/LoL-Player-Data-Analysis/players_info.csv", delimiter=";")
-playerinfoDF
