5279b3020ebb8373d416a0336349a3f390da3887
julius-ingeli <solidsnakesvk@gmail.com>
2022-04-14 17:34:42 +0200

    first draft kinda

---
+++ dataframes.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+
+playerinfoDF = pd.read_csv("/home/sldsnk/LoL-Player-Data-Analysis/players_info.csv", delimiter=";")
+playerinfoDF
